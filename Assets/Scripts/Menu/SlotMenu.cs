982ca8135c79633bd1a1a6d66686cb537cf788bf
julibamon <julibamon@alum.us.es>
2026-03-11 12:28:59 +0100

    Implementado sistema carga/nueva partida desde el menu

--- Assets/Scripts/Menu/SlotMenu.cs
+++ Assets/Scripts/Menu/SlotMenu.cs
@@ -1,7 +1,29 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class SlotMenu : MonoBehaviour
 {
+
+    public TMP_Text[] textSlotsArray; //los textos que se van a mostrar en la seleccion de slots
+    public Button[] buttonSlotArray; //los botones, para cambiar los fondos, el aspecto etc
+
+    void Start()
+    {
+        
+        for(int i=0; i<textSlotsArray.Length; i++)
+        {
+            if (SaveSystem.SlotExists(i))
+            {
+                textSlotsArray[i].text="Cargar partida";
+                buttonSlotArray[i].image.color=new Color(,,,);
+            }
+            else
+            {
+                textSlotsArray[i].text="Nueva partida";
+                buttonSlotArray[i].image.color=new Color(,,,); 
+            }
+    }
+    }
     public void SelectSlot(int slotId)
     {
         GameController.Instance.SlotSelectorPlayOrLoad(slotId);
