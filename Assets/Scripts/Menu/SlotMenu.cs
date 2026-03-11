eef631d71abc0f8ba44f2ea8272928321b226f39
julibamon <julibamon@alum.us.es>
2026-03-11 14:49:40 +0100

    Añadida funcionalidad menú borrar partida

--- Assets/Scripts/Menu/SlotMenu.cs
+++ Assets/Scripts/Menu/SlotMenu.cs
@@ -6,26 +6,38 @@ public class SlotMenu : MonoBehaviour
 
     public TMP_Text[] textSlotsArray; //los textos que se van a mostrar en la seleccion de slots
     public Button[] buttonSlotArray; //los botones, para cambiar los fondos, el aspecto etc
+    public Button[] buttonSlotDeleteArray; //botones de eliminar partida
 
     void Start()
     {
-        
-        for(int i=0; i<textSlotsArray.Length; i++)
-        {
-            if (SaveSystem.SlotExists(i))
-            {
-                textSlotsArray[i].text="Cargar partida";
-                buttonSlotArray[i].image.color=new Color(,,,);
-            }
-            else
-            {
-                textSlotsArray[i].text="Nueva partida";
-                buttonSlotArray[i].image.color=new Color(,,,); 
-            }
-    }
+        RefreshSlots();
     }
     public void SelectSlot(int slotId)
     {
         GameController.Instance.SlotSelectorPlayOrLoad(slotId);
+    }
+    void RefreshSlots()
+    {
+            for(int i=0; i<buttonSlotArray.Length; i++)
+        {
+            if (SaveSystem.SlotExists(i))
+            {
+                textSlotsArray[i].text="Cargar partida";
+                buttonSlotArray[i].image.color=Color.white;
+                buttonSlotDeleteArray[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                textSlotsArray[i].text="Vacío";
+                buttonSlotArray[i].image.color=new Color(0.4279091f, 0.3737985f, 0.4528302f, 1f);
+                buttonSlotDeleteArray[i].gameObject.SetActive(false); //desactivar botón borrar partida si no hay partida
+
+            }
+        }
+    }
+    public void DeleteSlot(int slotId)
+    {
+        SaveSystem.DeleteSlotData(slotId);
+        RefreshSlots(); //para que se actualice la UI al hacer el cambio (borrado de partida)
     }
 }
