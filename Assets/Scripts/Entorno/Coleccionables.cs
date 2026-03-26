5eea9457cda67b55a641f059468e879aa58a23e7
julibamon <julibamon@alum.us.es>
2026-03-26 09:47:55 +0100

    Objetos llaves y recetas por el mundo

---
+++ Assets/Scripts/Entorno/Coleccionables.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Coleccionables : MonoBehaviour
+
+{
+    private bool isPlayerHere = false;
+
+    public GameObject canvas; //texto encima del comestible
+
+    private PlayerController playerController;
+
+    public GameObject coleccionable;
+
+    public string nombreObjeto;
+
+    void Start()
+    {
+        if (GameController.Instance.currentSD.worldData.itemsListW.Contains(nombreObjeto)) //desactivamos el coleccionable si ya lo tenemos
+        {
+            coleccionable.SetActive(false);
+
+        }
+    }
+
+    void Update()
+    {
+        if(isPlayerHere && Input.GetKeyDown(KeyCode.E))
+        {
+            GetColeccionable();
+        }
+    }
+
+    public void GetColeccionable()
+    {
+
+        coleccionable.SetActive(false);
+        Debug.Log("Cogido el coleccionable"+ nombreObjeto);
+        GameController.Instance.currentSD.worldData.itemsListW.Add(nombreObjeto); // añadimos el coleccionable a la lista de objetos
+
+    }
+
+    //lógica de entrar en rango del objeto
+
+    void OnTriggerEnter2D(Collider2D colliderPlayer)
+    {
+        if (colliderPlayer.CompareTag("Player"))
+        {
+            isPlayerHere= true;
+            playerController = colliderPlayer.GetComponent<PlayerController>(); //para inicializar el player
+            canvas.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D colliderPlayer)
+    {
+        if (colliderPlayer.CompareTag("Player"))
+        {
+            isPlayerHere=false;
+            playerController=null;
+            canvas.SetActive(false);
+        }
+    }
+
+}
